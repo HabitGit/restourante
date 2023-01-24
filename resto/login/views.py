afb5c3cd65faace5d810e33fab2fc4f938b6c562
Igor <char.habit@gmail.com>
2023-01-24 20:27:27 +0400

    Add static files

--- resto/login/views.py
+++ resto/login/views.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.shortcuts import render
 
 
 @login_required
 def authorize(request):
     if request.user.is_authenticated:
-        return render(request, 'html/login_page.html')
+        return render(request, 'html/settings.html')
     else:
         return render(request, 'registration/login.html')
