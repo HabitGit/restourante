daec1a03ca9ef49a46459f114e41f7d147ee4143
Igor <char.habit@gmail.com>
2023-01-28 15:01:07 +0400

    rolled back because have some bags

--- resto/settings/views.py
+++ resto/settings/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 
 
+
+
 def settings(request):
     return render(request, 'html/settings.html')
-
-def profile(request):
-    return render(request, 'html/profile.html')
