2a0788adc9d5d1f6fb2067b0c133dc47ec7e6bc6
Igor <char.habit@gmail.com>
2023-01-28 15:02:15 +0400

    Create model Profile, add migrate, register settings app in settings.py, add Profile model in admin panel

--- resto/settings/models.py
+++ resto/settings/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    birthday = models.DateField()
+
+    def __str__(self):
+        return f'User {self.user}'
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
